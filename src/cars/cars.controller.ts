f75c57b60dc495821a3c7cf786e5699d8a91dda8
Chavez2208 <iscchavez26@gmail.com>
2023-10-26 18:44:41 -0600

    Update cars.controller.ts

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -16,7 +16,7 @@ export class CarsController {
     }
 
     @Get(':id')
-    getCarById(@Param('id', ParseIntPipe) id: number) {
+    getCarById(@Param('id', ParseIntPipe) id: string) {
         return this.carService.finOneBydId(id);
     }
 
